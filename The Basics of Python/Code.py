f78505f410c9a9e050fd010f93272286f03897e9
Ahmad Nabi Ahmadi <ahmadnabi.awlaki@gmail.com>
2019-09-24 03:15:46 -0700

    Add files via upload

---
+++ The Basics of Python/Code.py
@@ -0,0 +1,64 @@
+greeting = "Bruce"
+_myName = "Paul"
+Tim45 = "Good"
+Tim_Was_57 = "Hello"
+Greeting = "There"
+print(Tim_Was_57 + ' ' + greeting)
+
+age = 24
+print(age)
+
+print(greeting + age)
+#
+a = 12
+b = 3
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+
+for i in range(1, a / b):
+    print(i)
+#
+print(a + b / 3 - 4 * 12)
+print(8 / 2 * 3)
+print(8 * 3 / 2)
+print((((a + b) / 3) - 4) * 12)
+
+c = a + b
+d = c / 3
+e = d - 4
+print(e * 12)
+parrot = "Norwegian Blue"
+print(parrot)
+print(parrot[0])
+print(parrot[3])
+print(parrot[-1])
+print(parrot[0:6])
+print(parrot[:6])
+print(parrot[6:])
+print(parrot[-4:-2])
+print(parrot[0:6:2])
+print(parrot[0:6:3])
+
+number = "9,223,372,036,854,775,807"
+print(number[1::4])
+numbers = "1, 2, 3, 4, 5, 6, 7, 8, 9"
+print(numbers[0::3])
+string1 = "he's "
+string2 = "probably"
+print(string1 + string2)
+print("he's " "probably " "pining")
+print("Hello " * 5)
+print("Hello " * (5 + 4))
+print("Hello " * 5 + "4")
+
+today = "friday"
+print("day" in today)
+print("fri" in today)
+print("thur" in today)
+print("parrot" in "fjord")
+for i in range(len(parrot)):
+    print(parrot[i])
