d6904f6d8e0b1dc651f8e0bde4202052761d87f3
Ahmad Nabi Ahmadi <ahmadnabi.awlaki@gmail.com>
2019-09-24 03:59:10 -0700

    Add files via upload

---
+++ Using Databases in Python/tzcheck.py
@@ -0,0 +1,19 @@
+import sqlite3
+import pytz
+# import pickle
+
+
+db = sqlite3.connect("accounts.sqlite", detect_types=sqlite3.PARSE_DECLTYPES)
+
+# for row in db.execute("SELECT strftime('%Y-%m-%d %H:%M:%f', history.time, 'localtime') AS localtime,"
+#                       " history.account, history.amount FROM history ORDER BY history.time"):
+for row in db.execute("SELECT * FROM history"):
+    # utc_time = row[0]
+    print(row)
+    # pickled_zone = row[3]
+    # zone = pickle.loads(pickled_zone)
+    # zone = pytz.timezone("Australia/Adelaide")
+    # local_time = pytz.utc.localize(utc_time).astimezone(zone)
+    # print("{}\t{}\t{}".format(utc_time, local_time, local_time.tzinfo))
+
+db.close()
